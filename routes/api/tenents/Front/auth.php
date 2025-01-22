2093e13ceac61345bc845609223bfa9136414214
keincon <162288601+keincon@users.noreply.github.com>
2025-01-22 11:12:45 +0900

    API Auth

---
+++ routes/api/tenents/Front/auth.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Tenents\Front\AuthController;
+
+
+Route::prefix('frontend/{tenant}')
+    ->middleware(['basic.auth','set.maitainence','set.api.tenant'])  // Middleware to load tenant
+    ->name('tenant.')
+    ->group(function () {
+
+        Route::post('login', [AuthController::class, 'front_api_login'])->name('back.user.login');
+    });
+
+
+    
